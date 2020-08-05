0acdbc456c03d998e051af17ef51413ea4e30f5a
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-05 00:59:18 -0700

    update

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+}));
 
 
 const PORT = process.env.PORT || 5000;
