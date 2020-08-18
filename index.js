73f34ee63cfec45fea82fb28f6505c5090f84e2e
Shuaiqing Luo <shuaiqin@usc.edu>
2020-08-18 01:21:20 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
-}));
 
+mongoose.connect(keys.mongoURI);
+
+
+
+const app = express();
+require('./routes/authRoutes')(app);
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
